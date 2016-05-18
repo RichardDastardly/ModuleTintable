ee66bc2fe63b6709cdb6f0f46dbf54b948923c74
Richard Dastardly <karibanu@gmail.com>
2016-05-18 14:47:57 +0100

    First proper commit.

---
+++ Tinter/AssetLoader.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.IO;
+using System.Collections;
+using System.Collections.Generic;
+using System.Reflection;
+using UnityEngine;
+
+namespace Tinter
+{
+    [KSPAddon(KSPAddon.Startup.MainMenu, true)]
+    class AssetLoader : MonoBehaviour
+    {
+        // Shaders
+        private static Dictionary<string, Shader> Shaders = new Dictionary<string, Shader>();
+
+        public static bool shadersLoaded { get; private set; } = false;
+
+
+        // Keep the tinted replacement shaders named KSP/Tinted <oldshader> & we don't need a translator dictionary
+        public static Shader FetchRepacementShader( string KSPShader )
+        {
+            if (!shadersLoaded)
+                return null;
+
+            Shader rval;
+            TDebug.Print("Attempting to fetch replacement shader for " + KSPShader.Substring(4));
+            // assume shader string passed in is "KSP/<shader>" for now
+            if( Shaders.TryGetValue(KSPShader.Substring(4), out rval))
+            {
+                return rval;
+            }
+            return null;
+        }
+
+        // the proper loader is bugged! so surprised. Time to cobble together something, mostly cribbed
+        // from InfernalRobotics
+
+        private AssetBundle TABundle;
+
+        public IEnumerator LoadBundle(string FileLoc)
+        {
+            while (!Caching.ready)
+                yield return null;
+            using (WWW www = WWW.LoadFromCacheOrDownload(FileLoc, 1))
+            {
+                yield return www;
+                TABundle = www.assetBundle;
+
+                LoadBundledAssets();
+            }
+        }
+
+        private void LoadBundledAssets()
+        {
+            var BundleShaders = TABundle.LoadAllAssets<Shader>();
+
+            for( int i = 0; i < BundleShaders.Length; i++ )
+            {
+                if( BundleShaders[i] != null )
+                {
+                    string ShaderShortName = BundleShaders[i].name.Substring(11);
+                    // this is horribly inflexible, improve it later
+
+                    TDebug.Print("Loading shader " + BundleShaders[i].name + " as "+ShaderShortName);
+                    Shaders.Add(ShaderShortName, BundleShaders[i] );
+                }
+            }
+            shadersLoaded = true;
+        }
+
+        public void LoadBundleFromDisk( string FileLoc )
+        {
+            TABundle = AssetBundle.CreateFromFile(FileLoc);
+            LoadBundledAssets();
+        }
+
+        public void Start()
+        {
+            TDebug.Print("AssetLoader.Start() - grepping and loading shaders");
+
+            var assemblyFile = Assembly.GetExecutingAssembly().Location;
+            var BundlePath = "file://" + assemblyFile.Replace(new FileInfo(assemblyFile).Name, "").Replace("\\", "/");
+
+            TDebug.Print("Loading bundles from BundlePath: " + BundlePath);
+
+            //need to clean cache
+            Caching.CleanCache();
+
+            StartCoroutine(LoadBundle(BundlePath + "shaders.ksp"));
+
+            //     GetAssetDefinitionsWithType currently only returns one definition, and then bugs out and attempts to load the bundle again.
+            //           KSPAssets.Loaders.AssetLoader.LoadAssets(cbLoadShadersFromBundle,
+            //                   KSPAssets.Loaders.AssetLoader.GetAssetDefinitionsWithType("DLTD/Tinter/shaders", "Shader" ));
+        }
+
+        public void OnDestroy()
+        {
+            if( TABundle )
+            {
+                TDebug.Print("Unloading assets.");
+                TABundle.Unload(false);
+            }
+        }
+
+        // Currently redundant.
+        void cbLoadShadersFromBundle(KSPAssets.Loaders.AssetLoader.Loader Loader )
+        {
+            TDebug.Print("Attempting to load shaders...");
+            for (int i = 0; i < Loader.definitions.Length; ++i)
+            {
+                UnityEngine.Object obj = Loader.objects[i];
+                if (obj == null)
+                    continue;
+                Shader bundledShader = obj as Shader;
+                string ShaderName = Loader.definitions[i].name;
+                if (bundledShader != null && ShaderName != null) // is an empty string null?
+                {
+                    TDebug.Print("Loading shader " + ShaderName);
+                    Shaders.Add(ShaderName, bundledShader);
+                }
+            }
+        }
+    }
+}
---
+++ Tinter/Tinter.cs
@@ -0,0 +1,245 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace Tinter
+{
+    public static class TDebug
+    {
+        // debugging stuff from the start! how novel
+        public static string dbgTag = "[Tinter] ";
+
+        public static void Print(string dbgString)
+        {
+            Debug.Log(dbgTag + dbgString);
+        }
+
+        public static void Warn(string dbgString)
+        {
+            Debug.LogWarning(dbgTag + dbgString);
+        }
+
+        public static void Err(string dbgString)
+        {
+            Debug.LogError(dbgTag + dbgString);
+        }
+    }
+
+    public class PartTinter : PartModule
+    {
+
+        private bool active = false;
+        private bool needUpdate = false;
+        private bool needShaderReplacement = true;
+
+        private List<Material> ManagedMaterials = new List<Material>();
+
+        private void EditorPartEvent( ConstructionEventType cEvent, Part part )
+        {
+ //           if (cEvent != ConstructionEventType.PartTweaked) return;
+            TDebug.Print(part.name + "  event "+ cEvent.ToString());
+        }
+
+        private void EditorShipEvent( ShipConstruct cEvent )
+        {
+            TDebug.Print(part.name + " ship event "+ cEvent.ToString());
+        }
+
+        private void onTweakableChange(BaseField field, object what )
+        {
+            needUpdate = true;
+ //          UpdateShaderValues();
+//            TDebug.Print(part.name + " tweakable slider changed");
+        }
+
+        [KSPField(isPersistant = true, guiActive = false, guiActiveEditor = false, guiName = "Blend Value"),
+            UI_FloatRange(
+                affectSymCounterparts = UI_Scene.Editor,
+                minValue = 0,
+                maxValue = 255,
+                stepIncrement = 1,
+                scene = UI_Scene.Editor
+        )]
+        public float tintBaseTexVal = 0;
+
+        [KSPField(isPersistant = true, guiActive = false, guiActiveEditor = false, guiName = "Blend Band"),
+         UI_FloatRange(affectSymCounterparts = UI_Scene.Editor, minValue = 0, maxValue = 255, stepIncrement = 1, scene = UI_Scene.Editor)]
+        public float tintBaseTexVBand = 0;
+
+        [KSPField(isPersistant = true, guiActive = false, guiActiveEditor = false, guiName = "Blend Falloff"),
+         UI_FloatRange(affectSymCounterparts = UI_Scene.Editor, minValue = 0, maxValue = 255, stepIncrement = 1, scene = UI_Scene.Editor)]
+        public float tintBaseTexVFalloff = 0;
+
+        [KSPField(isPersistant = true, guiActive = false, guiActiveEditor = false, guiName = "Blend Saturation Threshold"),
+         UI_FloatRange(affectSymCounterparts = UI_Scene.Editor, minValue = 0, maxValue = 255, stepIncrement = 1, scene = UI_Scene.Editor)]
+        public float tintBaseTexSatThreshold = 0;
+
+        [KSPField(isPersistant = true, guiActive = false, guiActiveEditor = false, guiName = "Tint Hue"),
+          UI_FloatRange(affectSymCounterparts = UI_Scene.Editor, minValue = 0, maxValue = 255, stepIncrement = 1, scene = UI_Scene.Editor)]
+        public float tintHue = 0;
+
+        [KSPField(isPersistant = true, guiActive = false, guiActiveEditor = false, guiName = "Tint Saturation"),
+          UI_FloatRange(affectSymCounterparts = UI_Scene.Editor, minValue = 0, maxValue = 255, stepIncrement = 1, scene = UI_Scene.Editor)]
+        public float tintSaturation = 0;
+
+        [KSPField(isPersistant = true, guiActive = false, guiActiveEditor = false, guiName = "Tint Value"),
+          UI_FloatRange(affectSymCounterparts = UI_Scene.Editor, minValue = 0, maxValue = 255, stepIncrement = 1, scene = UI_Scene.Editor)]
+        public float tintValue = 0;
+
+        private void ToggleFields( bool flag )
+        {
+            for ( int i = 0; i< Fields.Count; i++ )
+            {
+                if( Fields[i].uiControlEditor != null )
+                {
+                    Fields[i].guiActiveEditor = flag;
+                }
+            }
+        }
+
+        private void TweakFieldSetup()
+        {
+            for( int i = 0; i < Fields.Count; i++ )
+            {
+                var uiField = Fields[i].uiControlEditor as UI_FloatRange;
+                if (uiField != null) // all parts with ui components are currently sliders we want to trap changes for
+                {
+  //                  TDebug.Print("Setting onFieldChange for " + Fields[i].guiName);
+                    uiField.onFieldChanged = onTweakableChange;
+                }
+            }
+        }
+
+        private void TraverseAndReplaceShaders()
+        {
+            if (!AssetLoader.shadersLoaded)
+                return;
+
+            if (!needShaderReplacement)
+            {
+                TDebug.Print("Apparently " + part.name + " doesn't need shader replacement");
+                return;
+            }
+
+            var Materials = new List<Material>();
+            
+            // messy messy, tidy
+            foreach ( Renderer r in GetComponentsInChildren<MeshRenderer>(true))
+            {
+                Materials.AddRange(r.materials);
+            }
+            foreach( Material m in Materials.ToArray())
+            {
+                TDebug.Print(part.name + " material " + m.name);
+                var replacementShader = AssetLoader.FetchRepacementShader(m.shader.name);
+                if (replacementShader)
+                {
+                    active = true;
+                    TDebug.Print(part.name+ " Replacing shader " + m.shader.name + " with " + replacementShader.name);
+                    m.shader = replacementShader;
+                    ManagedMaterials.Add(m);
+                    needUpdate = true;
+                }
+            }
+            ToggleFields(active);
+            needShaderReplacement = false;
+        }
+
+  //      float _TintHue;
+  //      float _TintSat;
+   //     float _TintVal;
+   //     float _TintPoint;
+   //     float _TintBand;
+   //     float _TintFalloff;
+   //     float _TintSatThreshold;
+
+        private float SliderToShaderValue( float v )
+        {
+            return v / 255;
+        }
+
+        private void UpdateShaderValues()
+        {
+            foreach ( Material m in ManagedMaterials.ToArray())
+            {
+                TDebug.Print(part.name + "Updating material " + m.name);
+                m.SetFloat("_TintPoint", SliderToShaderValue(tintBaseTexVal));
+                m.SetFloat("_TintBand", SliderToShaderValue(tintBaseTexVBand));
+                m.SetFloat("_TintFalloff", SliderToShaderValue(tintBaseTexVFalloff));
+                m.SetFloat("_TintHue", SliderToShaderValue(tintHue));
+                m.SetFloat("_TintSat", SliderToShaderValue(tintSaturation));
+                m.SetFloat("_TintVal", SliderToShaderValue(tintValue));
+                m.SetFloat("_TintSatThreshold", SliderToShaderValue(tintBaseTexSatThreshold));
+            }
+        }
+
+        public void Update()
+        {
+            if( needUpdate )
+            {
+                needUpdate = false;
+  //              TDebug.Print("Update() - updating shader values");
+                UpdateShaderValues();
+                // update shader values
+            }
+        }
+
+        public override void OnStart(StartState state)
+        {
+            base.OnStart(state);
+            TweakFieldSetup();
+            TraverseAndReplaceShaders();
+            TDebug.Print("OnStart() [" + part.name + "]" );
+        }
+
+        public void Start()
+        {
+            this.part.OnEditorAttach += new Callback(OnEditorAttach);
+  //          GameEvents.onEditorPartEvent.Add(EditorPartEvent);
+  //          GameEvents.onEditorShipModified.Add(EditorShipEvent);
+            TDebug.Print("Start() [" + this.part.name + "]");
+        }
+
+        private void OnDestroy()
+        {
+  //          GameEvents.onEditorPartEvent.Remove(EditorPartEvent);
+ //           GameEvents.onEditorShipModified.Remove(EditorShipEvent);
+        }
+
+
+        public override void OnUpdate()
+        {
+            if( needUpdate )
+            {
+                needUpdate = false;
+                TDebug.Print("OnUpdate(): needUpdate set " + this.part.name);
+            }
+        }
+
+        public void OnEditorAttach()
+        {
+            TDebug.Print("Editor - attach [" + this.part.name + "]");
+        }
+
+        public override void OnSave(ConfigNode node)
+        {
+            base.OnSave(node);
+            TDebug.Print("OnSave() [" + this.part.name + "]");
+        }
+
+        public override void OnLoad(ConfigNode node)
+        {
+            base.OnLoad(node);
+            TDebug.Print("OnLoad() [" + this.part.name + "]");
+            TraverseAndReplaceShaders();
+        }
+
+        public void Setup()
+        {
+            TDebug.Print("Setup() [" + this.part.name + "]");
+        }
+     }
+}
